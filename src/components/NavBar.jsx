7d814d5f67b9d25b4662928ac5730f1d1a899dce
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-12-20 12:55:12 -0500

    Implement Porjects arrow

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className="mx-[30px] md:mx-[130px] font-helvetica py-[30px] md:py-[50px]">
+    <nav className=" flex justify-between mx-[30px] md:mx-[130px] font-helvetica py-[30px] md:py-[50px]">
       <div className="text-3xl md:text-5xl flex gap-[8px] md:gap-[12px]">
         <h1 className="font-bold">AQ</h1>
         <div className="flex items-center">
@@ -31,6 +31,24 @@ const NavBar = () => {
           </svg>
         </div>
       </div>
+      <ul className="hidden md:flex justify-end items-center gap-10">
+        <li>HOME</li>
+        <li className="flex items-center gap-2">
+          <span>PROJECTS</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width=".8em"
+            height=".8em"
+            viewBox="0 0 17 13"
+            fill="none"
+          >
+            <path d="M1 1.5L8.5 10.5L16 1.5" stroke="black" stroke-width="2" />
+          </svg>
+        </li>
+        <li>ABOUT</li>
+        <li>PLAY</li>
+      </ul>
+      <div className="md:hidden">MobileMenu</div>
     </nav>
   );
 };
