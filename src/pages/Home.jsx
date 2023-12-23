0e00b80213d7b9e7da88df0ae6e51f28b635276e
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-12-22 21:10:16 -0500

    Lay out project links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Beach2 from "../assets/Beach2.png";
+import Beach1 from "../assets/Beach1.png";
+import HomeImg from "../assets/Home.png";
+import MatchImg from "../assets/Match.png";
 
 const Home = () => {
   return (
-    <div className="mx-[50px] md:mx-[130px] lg:mx-[200px] 2xl:mx-[340px] font-garamond">
+    <div className="mx-[50px] md:mx-[130px] lg:mx-[200px] 2xl:mx-[340px] font-garamond max-w-full">
       <section className="font-garamond pt-[75px] lg:pb-[150px] pb-[100px] border-b border-b-black/40">
         <h2 className="font-helvetica md:text-5xl text-4xl lg:text-8xl font-bold text-center">
           Aleynna Quinones
@@ -31,8 +35,66 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section>Work 1</section>
-      <section>Work 2</section>
+      <section className="font-garamond pt-[100px] lg:pb-[150px] pb-[100px] border-b border-b-black/40">
+        <div className="flex flex-col-reverse lg:flex-row-reverse justify-start items-center gap-[30px] lg:gap-[65px] mb-[75px] lg:mb-[145px]">
+          <div className="text-center md:text-left flex items-center lg:items-start flex-col gap-[10px] lg:gap-[15px]">
+            <h6 className="text-center md:text-left font-thin font-helvetica text-[20px] lg:text-[24px]">
+              ACADEMIC PROJECT
+            </h6>
+            <h3 className="text-center md:text-left leading-none font-helvetica text-[30px] lg:text-[64px] font-bold">
+              Connection Café
+            </h3>
+            <p className="text-center md:text-left text-[24px] lg:text-[24px] font-thin">
+              Write Something Else
+            </p>
+            <button className="font-helvetica font-normal px-12 py-3 rounded-full border-2 border-solid border-black">
+              Learn More
+            </button>
+          </div>
+          <div className="flex gap-[15px] lg:max-w-[calc(100%-415px)] max-w-full">
+            <img
+              className="min-w-[90px] max-w-[200px] h-auto"
+              src={HomeImg}
+              alt="Snackshop Home"
+            />
+            <img
+              className="min-w-[90px] max-w-[200px] h-auto"
+              src={MatchImg}
+              alt="Snackshop Menu"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col-reverse lg:flex-row justify-start items-center gap-[30px] lg:gap-[65px]">
+          <div className="flex items-center lg:items-start flex-col gap-[10px] lg:gap-[15px]">
+            <h6 className="text-center md:text-left font-thin font-helvetica text-[20px] lg:text-[24px]">
+              ACADEMIC PROJECT
+            </h6>
+            <h3 className="text-center md:text-left leading-none font-helvetica text-[30px] lg:text-[64px] font-bold">
+              BeachSide
+              <br />
+              SnackShop
+            </h3>
+            <p className="text-center md:text-left text-[24px] lg:text-[24px] font-thin">
+              Write Something Else
+            </p>
+            <button className="font-helvetica font-normal px-12 py-3 rounded-full border-2 border-solid border-black">
+              Learn More
+            </button>
+          </div>
+          <div className="flex gap-[15px] lg:max-w-[calc(100%-415px)] max-w-full">
+            <img
+              className="min-w-[90px] max-w-[200px] h-auto"
+              src={Beach1}
+              alt="Snackshop Home"
+            />
+            <img
+              className="min-w-[90px] max-w-[200px] h-auto"
+              src={Beach2}
+              alt="Snackshop Menu"
+            />
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
