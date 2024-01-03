ddda9530745f2c6e32b4ac08dfd2078ba91d3410
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2024-01-03 13:01:30 -0500

    Add blobs to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import Blobs from "../assets/BLOBS.png";
 
 import selfies from "../assets/selfies.png";
 
 const About = () => {
   return (
     <div className="py-[50px] md:py-[110px] mx-[50px] md:mx-[130px] lg:mx-[200px] 2xl:mx-[340px] font-garamond max-w-full flex flex-col md:flex-row-reverse md:gap-[55px] gap-[30px] items-center">
-      <div className="flex justify-center items-center min-w-[90px] max-w-[200px] md:max-w-[350px]">
-        <img className=" h-auto" src={selfies} alt="Aleynna 3" />
+      <div className="relative flex justify-center items-center min-w-[90px] max-w-[200px] md:max-w-[350px]">
+        <img className="relative z-10 h-auto" src={selfies} alt="Aleynna 3" />
+        <img className="absolute scale-150 z-0 h-auto" src={Blobs} alt="Blobs" />
       </div>
 
       <div className="flex flex-col gap-5 lg:gap-10 items-start">
