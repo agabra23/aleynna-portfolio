a8077b7ef503ada7d001409410f33dc40c0347e9
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2024-01-05 12:00:02 -0500

    ADjust positioning of section titles

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -12,114 +12,74 @@ import IC from "../assets/IC.png";
 
 const Play = () => {
   return (
-    <div className="py-[60px] md:py-[110px] mx-[50px] md:mx-[130px] lg:mx-[200px] 2xl:mx-[340px] font-helvetica max-w-full flex flex-col gap-[75px] md:gap-[110px] items-center lg:items-start">
-      <div className="flex flex-col gap-[40px]">
-        <h4 className="text-[30px] font-normal text-center lg:text-left">
-          Acrylic Painting
-        </h4>
-        <div className="flex flex-wrap justify-center lg:justify-start gap-x-[175px] gap-y-[75px] md:gap-y-[150px]">
-          <div className="flex flex-col justify-center items-center gap-[20px]">
-            <img
-              className="object-cover max-w-[430px] h-auto align-middle rounded-[25px]"
-              src={DS_2}
-              alt=""
-            />
-            <h6 className="font-thin text-[16px] text-center uppercase">
+    <div className="py-[135px] md:py-[110px] mx-[50px] md:mx-[130px] lg:mx-[200px] 2xl:mx-[340px] font-helvetica max-w-full flex flex-col gap-[75px] md:gap-[110px] items-center">
+      <div className="flex-col flex items-center gap-[185px]">
+        <div className=" flex flex-wrap gap-x-[175px] gap-y-[75px] justify-center">
+          <div className="relative flex-col flex gap-[20px] basis-[300px]">
+            <h4 className="absolute left-1/2 w-max -translate-x-1/2 text-center min-[1175px]:text-left min-[1175px]:translate-x-0 min-[1175px]:left-0 -top-[75px] font-normal text-[30px] lg:self-start">
+              Acrylic Painting
+            </h4>
+            <img className="object-cover" src={DS_2} alt="" />
+            <h6 className="uppercase text-[16px] font-thin text-center">
               Dear Somebody #2
             </h6>
           </div>
-          <div className="flex flex-col justify-center items-center gap-[20px]">
-            <img
-              className="object-cover max-w-[430px] h-auto align-middle rounded-[25px]"
-              src={DS_1}
-              alt=""
-            />
-            <h6 className="font-thin text-[16px] text-center uppercase">
+          <div className="flex-col flex gap-[20px] basis-[300px]">
+            <img src={DS_1} alt="" />
+            <h6 className="uppercase text-[16px] font-thin text-center">
               Dear Somebody #1
             </h6>
           </div>
-          <div className="flex flex-col justify-center items-center gap-[20px]">
-            <img
-              className="object-cover max-w-[430px] h-auto align-middle rounded-[25px]"
-              src={GC}
-              alt=""
-            />
-            <h6 className="font-thin text-[16px] text-center uppercase">
+          <div className="flex-col flex gap-[20px] basis-[300px]">
+            <img src={GC} alt="" />
+            <h6 className="uppercase text-[16px] font-thin text-center">
               Guilty Conscience
             </h6>
           </div>
-          <div className="flex flex-col justify-center items-center gap-[20px]">
-            <img
-              className="object-cover max-w-[430px] h-auto align-middle rounded-[25px]"
-              src={MG}
-              alt=""
-            />
-            <h6 className="font-thin text-[16px] text-center uppercase">
+          <div className="flex-col flex gap-[20px] basis-[300px]">
+            <img src={MG} alt="" />
+            <h6 className="uppercase text-[16px] font-thin text-center">
               The Male Gaze
             </h6>
           </div>
         </div>
-      </div>
-      <div className="flex flex-col gap-[40px]">
-        <h4 className="text-[30px] font-normal text-center lg:text-left">
-          Graphite and Color Pencil
-        </h4>
-        <div className="flex flex-wrap justify-center lg:justify-start gap-x-[175px] gap-y-[75px] md:gap-y-[150px]">
-          <div className="flex flex-col justify-center items-center gap-[20px]">
-            <img
-              className="object-cover max-w-[430px] h-auto align-middle rounded-[25px]"
-              src={FO}
-              alt=""
-            />
-            <h6 className="font-thin text-[16px] text-center uppercase">
+        <div className=" flex flex-wrap gap-x-[175px]">
+          <div className="relative flex-col flex gap-[20px] basis-[300px]">
+            <h4 className="absolute left-1/2 w-max -translate-x-1/2 text-center min-[1175px]:text-left min-[1175px]:translate-x-0 min-[1175px]:left-0 -top-[75px] font-normal text-[30px] lg:self-start">
+              Graphite and Color Pencil
+            </h4>
+            <img className="object-cover" src={FO} alt="" />
+            <h6 className="uppercase text-[16px] font-thin text-center">
               Frank Ocean
             </h6>
           </div>
         </div>
-      </div>
-      <div className="flex flex-col gap-[40px]">
-        <h4 className="text-[30px] font-normal text-center lg:text-left">
-          Digital Art
-        </h4>
-        <div className="flex flex-wrap justify-center lg:justify-start gap-x-[175px] gap-y-[75px] md:gap-y-[150px]">
-          <div className="flex flex-col justify-center items-center gap-[20px]">
-            <img
-              className="object-cover max-w-[430px] h-auto align-middle rounded-[25px]"
-              src={PDT}
-              alt=""
-            />
-            <h6 className="font-thin text-[16px] text-center uppercase">
-              Please Do Not Touch The Artwork
+        <div className=" flex flex-wrap gap-x-[175px] gap-y-[75px] justify-center">
+          <div className="relative flex-col flex gap-[20px] basis-[300px]">
+            <h4 className="absolute left-1/2 w-max -translate-x-1/2 text-center min-[1175px]:text-left min-[1175px]:translate-x-0 min-[1175px]:left-0 -top-[75px] font-normal text-[30px] lg:self-start">
+              Digital Art
+            </h4>
+            <img className="object-cover" src={PDT} alt="" />
+            <h6 className="uppercase text-[16px] font-thin text-center">
+              Please Don't Touch The Artwork
             </h6>
           </div>
-          <div className="flex flex-col justify-center items-center gap-[20px]">
-            <img
-              className="object-cover max-w-[430px] h-auto align-middle rounded-[25px]"
-              src={IC}
-              alt=""
-            />
-            <h6 className="font-thin text-[16px] text-center uppercase">
-              My Inner Child
-            </h6>
-          </div>
-          <div className="flex flex-col justify-center items-center gap-[20px]">
-            <img
-              className="object-cover max-w-[430px] h-auto align-middle rounded-[25px]"
-              src={WII}
-              alt=""
-            />
-            <h6 className="font-thin text-[16px] text-center uppercase">
+          <div className="flex-col flex gap-[20px] basis-[300px]">
+            <img className="object-cover" src={WII} alt="" />
+            <h6 className="uppercase text-[16px] font-thin text-center">
               What Is It
             </h6>
           </div>
-          <div className="flex flex-col justify-center items-center gap-[20px]">
-            <img
-              className="object-cover max-w-[430px] h-auto align-middle rounded-[25px]"
-              src={IMH}
-              alt=""
-            />
-            <h6 className="font-thin text-[16px] text-center uppercase">
+          <div className="flex-col flex gap-[20px] basis-[300px]">
+            <img className="object-cover" src={IMH} alt="" />
+            <h6 className="uppercase text-[16px] font-thin text-center">
               In My Head
+            </h6>
+          </div>
+          <div className="flex-col flex gap-[20px] basis-[300px]">
+            <img className="object-cover" src={IC} alt="" />
+            <h6 className="uppercase text-[16px] font-thin text-center">
+              My Inner Child
             </h6>
           </div>
         </div>
